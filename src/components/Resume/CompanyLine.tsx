48e5448dc48452be68ca3b04f77ad811e2b894fa
Lauren Ashley <laurenashley@macbooks-MBP.attlocal.net>
2022-09-28 17:56:15 -0400

    modify display

--- src/components/Resume/CompanyLine.tsx
+++ src/components/Resume/CompanyLine.tsx
@@ -1,16 +1,30 @@
 import * as React from "react";
-import { Avatar, Grid, Typography } from "@mui/material";
+import { Avatar, Box, Grid, Typography } from "@mui/material";
 import { CompanyProps } from "../../types/company";
 
 export default function CompanyLine(props: CompanyProps) {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Avatar alt={props.name} src={props.src} />
-        <Typography variant="h6">
-          {props.name}, {props.year}
-        </Typography>
-      </Grid>
+      <Box m={2} p={2}>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Avatar
+            alt={props.name}
+            src={props.src}
+            sx={{ width: 32, height: 32 }}
+          />
+          <span>&nbsp;</span>
+          <Typography variant="h6">
+            {props.name}, {props.year}
+          </Typography>
+        </Grid>
+      </Box>
     </Grid>
   );
 }
